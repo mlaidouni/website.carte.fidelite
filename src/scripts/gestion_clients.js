8f8fa50b127b20e81b1a39b4d002fd3e1c8fd7d5
laidouni <mohamed.laidouni@etu.u-paris.fr>
2024-03-31 18:37:09 +0200

    src updated: premières architecture créée

---
+++ src/scripts/gestion_clients.js
@@ -0,0 +1,9 @@
+// TODO: voir la liste des clients
+
+// TODO: ajouter client
+
+// TODO: supprimer client
+
+// TODO: modifier client
+
+// TODO: search client
---
+++ src/scripts/serveur.js
@@ -0,0 +1,20 @@
+/* Gestion des Routes */
+
+// GET client/connexion
+// TODO: redirect client/compte
+
+// GET client/compte
+// TODO: render compte_client.ejs
+
+// GET client/achat
+// TODO: render achat_cadeaux.ejs
+
+// GET gerante/connexion
+// TODO: redirect gerante/compte
+
+// GET gerante/compte
+// TODO: render compte_gerante.ejs
+
+// TODO: ajouter les requêtes en POST
+
+// Lancer le serveur...
