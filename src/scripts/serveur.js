160400ab9f6719cafbbd78fcaa75db7a2724e585
laidouni <mohamed.laidouni@etu.u-paris.fr>
2024-03-31 19:35:35 +0200

    src/scripts/serveur.js updated: ajout de la route '/' pour tester la page 'accueil.html'

--- src/scripts/serveur.js
+++ src/scripts/serveur.js
@@ -1,7 +1,31 @@
+// Import des modules express et path
+const express = require("express");
+const path = require("path");
+
+// Création et configuration du serveur
+const server = new express();
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
 /* Gestion des Routes */
+
+// Notification sur le terminal pour toutes les requêtes
+server.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// GET /
+// TODO: render accueil.html
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../pages", "accueil.html"));
+});
 
 // GET client/connexion
 // TODO: redirect client/compte
+server.get("/client/connexion", (req, res) => {
+  res.send("Ceci est la page de connexion CLIENT !");
+});
 
 // GET client/compte
 // TODO: render compte_client.ejs
@@ -11,6 +35,9 @@
 
 // GET gerante/connexion
 // TODO: redirect gerante/compte
+server.get("/gerante/connexion", (req, res) => {
+  res.send("Ceci est la page de connexion GERANTE !");
+});
 
 // GET gerante/compte
 // TODO: render compte_gerante.ejs
@@ -18,3 +45,8 @@
 // TODO: ajouter les requêtes en POST
 
 // Lancer le serveur...
+// Start the server
+const port = 8080;
+server.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}/`);
+});
